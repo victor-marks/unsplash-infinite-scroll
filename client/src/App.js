b94e9d4c08db4781eeb6bb16fee47fcdaad46c05
Victor Marks <vjmarks@gmail.com>
2019-07-18 16:02:29 -0700

    displayed 30 Image components inside Images component at a time

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Images from './components/Images';
+import './App.css';
+
+function App() {
+  return (
+    <div id="root">
+      <div className="hero is-fullheight is-bold is-info">
+        <div className="hero-body">
+          <div className="container">
+            <div className="header content">
+              <h2 className="subtitle is-6">Code Challenge #16</h2>
+              <h1 className="title is-1">
+                Infinite Scroll Unsplash Code Challenge
+              </h1>
+            </div>
+            <Images />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
